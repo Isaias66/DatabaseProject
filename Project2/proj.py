5cb984af8a7698a1e1e2b9d74ee0b7663f6201bd
Isaias Melendez <acemelen@gmail.com>
2022-10-10 18:02:06 -0600

    Updated Proj.py
    Added minor comments and error checks to fit within the bonds of the rubric

--- Project2/proj.py
+++ Project2/proj.py
@@ -9,23 +9,22 @@ Purpose: This program will receive user input through the function main() parame
          correlated information depending on the values entered. This information will come from the
          database schema that was given in Project 1.
 
-Pre-Conditions: 
-
-
-Post-Conditions:
-
-
 """
 import mysql.connector
 import sys
 
-#Establish database connection to cs482502
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "dbuser",
-    passwd= "lwilldowell",
-    database="cs482502"
-    )
+#Establish database connection to cs482502, this is hardcoded in the program
+#We also check if the connection failed to ensure that the program works
+try:
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "dbuser",
+        passwd= "lwilldowell",
+        database="cs482502"
+        )
+except mysql.connector.Error as err:
+    print("Something went wrong: {}".format(err))
+
 
 #@Param: A string variable containing a street name
 #@Output: All sites that are on the given street, each
@@ -35,18 +34,23 @@ def queryOne(arg):
     mycursor = mydb.cursor()
     param = arg
     
-    sql = "SELECT * FROM Site WHERE address like '%" + param + "%'"
+    sql = "SELECT * FROM Site WHERE address like '% %s, %'"
     val = (param,)
 
     mycursor.execute(sql)
     record = mycursor.fetchall()
 
-    for row in record:
-        print("Site Code: ", row[0],)
-        print("Type: ", row[1])
-        print("Address: ", row[2])
-        print("Phone No. : ", row[3], "\n")
+    #Check if results exist for given query
+    if mycursor.rowcount == 0:
+        print("No results found for given query\n")
+    else:
+        for row in record:
+            print("Site Code: ", row[0],)
+            print("Type: ", row[1])
+            print("Address: ", row[2])
+            print("Phone No. : ", row[3], "\n")
 
+    #Close SQL connection
     if (mydb.is_connected()):
         mydb.close()
         mycursor.close()
@@ -54,7 +58,7 @@ def queryOne(arg):
 
 
 #@Param: A string variable containing a scheduler system
-#@Output: 
+#@Output: Displays serial no, model no, and technical support name for a given scheduler system
 def queryTwo(arg):
     mycursor = mydb.cursor()
     param = arg
@@ -65,11 +69,16 @@ def queryTwo(arg):
     mycursor.execute(sql,val)
     record = mycursor.fetchall()
 
-    for row in record:
-        print("Serial No: ", row[0], )
-        print("Model No: ", row[1])
-        print("Technical Support Name: ", row[2], "\n")
+    #Check if results exist for given query
+    if mycursor.rowcount == 0:
+        print("No results found for given query\n")
+    else:
+        for row in record:
+            print("Serial No: ", row[0], )
+            print("Model No: ", row[1])
+            print("Technical Support Name: ", row[2], "\n")
 
+    #Close SQL connection
     if (mydb.is_connected()):
         mydb.close()
         mycursor.close()
@@ -77,7 +86,8 @@ def queryTwo(arg):
 
 
 #@Param: None
-#@Output: 
+#@Output: Displays the distinct names of aall salesmand and frequency of salesmen
+#        with the specified name
 def queryThree():
     mycursor = mydb.cursor()
 
@@ -90,12 +100,24 @@ def queryThree():
     print("-------------")
 
     record = mycursor.fetchall()
+
+    #Check if results exist for given query
+    if mycursor.rowcount == 0:
+        print("No results found for given query\n")
+    #else:
     
     for row in record:        
         print(row[1])
 
-#@Param: 
-#@Output:
+    #Close SQL connection
+    if (mydb.is_connected()):
+        mydb.close()
+        mycursor.close()
+        print("MySQL connection is closed")
+
+
+#@Param: A phone number in the format '575-111-1111', will only accept a phone number in this format
+#@Output: All clients with the specified phone number. Will print out the client's ID, Name, Phone, and Address
 def queryFour(arg):
     mycursor = mydb.cursor()
     param = arg
@@ -106,12 +128,17 @@ def queryFour(arg):
     mycursor.execute(sql,val)
     record = mycursor.fetchall()
 
-    for row in record:
-        print("clientID ", row[0], )
-        print("Name: ", row[1])
-        print("Phone: ", row[2])
-        print("Address: ", row[3], "\n")
+    #Check if results exist for given query
+    if mycursor.rowcount == 0:
+        print("No results found for given query\n")
+    else:
+        for row in record:
+            print("clientID ", row[0], )
+            print("Name: ", row[1])
+            print("Phone: ", row[2])
+            print("Address: ", row[3], "\n")
 
+    #Close SQL connection
     if (mydb.is_connected()):
         mydb.close()
         mycursor.close()
@@ -119,25 +146,33 @@ def queryFour(arg):
 
 
 #@Param: None
-#@Output: 
+#@Output: Displays the Administrators empId, Name, and total working hours in ascending order
+#        of total working hours
 def queryFive():
     mycursor = mydb.cursor()
-    
+
+    #This SQL statement needs to be reviewed WIP
     sql = "SELECT b.empId, b.name, a.hours FROM AdmWorkHours as a, Administrator as b WHERE a.empId = b.empId ORDER BY a.hours asc;"
 
     mycursor.execute(sql)
     record = mycursor.fetchall()
 
-    # NOTE: I am unsure if this is the correct info for the total hours, this may need to be reviewed
-    for row in record:
-        print("EmpId: ", row[0], )
-        print("Name: ", row[1])
-        print("Hours: ", row[2], "\n")
+    #Check if results exist for given query
+    if mycursor.rowcount == 0:
+        print("No results found for given query\n")
+    else:
+        # NOTE: I am unsure if this is the correct info for the total hours, this may need to be reviewed WIP
+        for row in record:
+            print("EmpId: ", row[0], )
+            print("Name: ", row[1])
+            print("Hours: ", row[2], "\n")
 
+    #Close SQL connection
     if (mydb.is_connected()):
         mydb.close()
         mycursor.close()
         print("MySQL connection is closed")   
+
 
 #@Param: A model Number, string 
 #@Output: The names of the technical supports who work on the specified model.
@@ -151,16 +186,23 @@ def querySix(arg):
     mycursor.execute(sql,val)
     record = mycursor.fetchall()
 
-    for row in record:
-        print("Name of Tech. Support: ", row[0], "\n")
+    #Check if results exist for given query
+    if mycursor.rowcount == 0:
+        print("No results found for given query\n")
+    else:
+        for row in record:
+            print("Name of Tech. Support: ", row[0], "\n")
 
+    #Close SQL connection
     if (mydb.is_connected()):
         mydb.close()
         mycursor.close()
         print("MySQL connection is closed")
 
+
 #@Param: None
-#@Output: 
+#@Output: Displays each salesman's name, and average commision rate. This is ordered in
+#        decending order of the salesman average commision rate.
 def querySeven():
     mycursor = mydb.cursor()
 
@@ -170,11 +212,16 @@ def querySeven():
     mycursor.execute(sql)
     record = mycursor.fetchall()
 
-    # NOTE: The output is only one salesman which is not correct, WIP
-    for row in record:
-        print("Name: ", row[0], )
-        print("Avg.CommisionRate: ", row[1], "\n")
+    #Check if results exist for given query
+    if mycursor.rowcount == 0:
+        print("No results found for given query\n")
+    else:
+        # NOTE: The output is only one salesman which is not correct, WIP
+        for row in record:
+            print("Name: ", row[0], )
+            print("Avg.CommisionRate: ", row[1], "\n")
 
+    #Close SQL connection
     if (mydb.is_connected()):
         mydb.close()
         mycursor.close()
@@ -182,31 +229,40 @@ def querySeven():
 
 
 #@Param: None
-#@Output: 
+#@Output: Displays the total number of administrators, salesmen, and technical supports
 def queryEight():
     mycursor = mydb.cursor()
 
-    ##NOTE: WIP this is not the correct SQL statement
-    sql = ""
+    sql = "SELECT count(DISTINCT a.empId), count(DISTINCT s.empId), count(DISTINCT t.empID) FROM TechnicalSupport as t, Administrator as a, Salesman as s;"
 
     mycursor.execute(sql)
     record = mycursor.fetchall()
 
-    # NOTE:WIP
-    for row in record:
-        print("")
+    print("Role          Cnt")
+    print("----------------")
+    
+    #Check if results exist for given query
+    if mycursor.rowcount == 0:
+        print("No results found for given query\n")
+    else:
+        for row in record:
+            print("Administrator: ", row[0])
+            print("Salesman:      ", row[1])
+            print("Technicians:   ", row[2], "\n")
 
+    #Close SQL connection
     if (mydb.is_connected()):
         mydb.close()
         mycursor.close()
         print("MySQL connection is closed")   
+
 
 def main():
 
     #This will ignore 'proj.py' and recognize the arugments given after
     #the python script 
     args = sys.argv[1:]
-
+    
     #Question Num: 1
     if len(args) == 2 and args[0] == '1':
         queryOne(args[1])
