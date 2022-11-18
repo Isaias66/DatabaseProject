dc79197f1e7b11904e240dc23e6c49354fa7ec28
Isaias Melendez <acemelen@gmail.com>
2022-11-18 08:50:07 -0700

    Cleaned up minor issues
    Refactored code to have some preconditions rather than using concatenation

--- Project3/project3.py
+++ Project3/project3.py
@@ -1,41 +1,88 @@
+"""
+CS-482
+Authors: Miguel Fernandez, Camika Leiva, Isaias Melendez
+11-18-22
+
+Version: Python 3.10.2
+
+Purpose: 
+
+"""
 import sys
 import mysql.connector
 
-def DisplayModel():
-    print("The format for models is: (modelNo, width, height, weight, depth, screenSize)")
-    print("-----------------------------------------------------------------------------")
-    mycursor.execute("SELECT * FROM Model;")
-    for x in mycursor:
-        print(x)
-
+#Establish database connection to cs482502, this is hardcoded in the program
+#We also check if the connection failed to ensure that the program works and
+#Prompt users to enter information through a command line interface
 def Login():
+
+    #Display basic cmd information for user to type in information
     print("Login:")
     hostVar = input("Enter your host: ")
     userVar = input("Enter your username: ")
     passwdVar = input("Enter your password: ")
     dataVar = input("Enter the name of the database you want to use: ")
+
+    #Check if the connection was successful
     global mydb
     mydb = mysql.connector.connect(
         host = hostVar,
         user = userVar,
         password = passwdVar,
         database = dataVar
-    )
-    
+    )        
+
+    #DB connection was successful so we may create a cursor
     if(mydb.is_connected()):
         print("Great, you are connected!")
         global mycursor
         mycursor = mydb.cursor()
+        
+    #Else the credentials were incorrect and we redirect back to the login page
     else:
         print("We believed you typed something wrong, try again.")
         login()
 
+
+def DisplayModel():
+    print("The format for models is: (modelNo, width, height, weight, depth, screenSize)")
+    print("-----------------------------------------------------------------------------")
+
+    sql = "SELECT * FROM Model;"
+    mycursor.execute(sql)
+    record = mycursor.fetchall()
+
+    #Check if results exist for the given query
+    if mycursor.rowcount == 0:
+        print("No results found for given query\n")
+    else:
+        for row in record:
+            print("ModelNo: ", row[0],)
+            print("Width: ", row[1],)
+            print("Height: ", row[2],)
+            print("Weight: ", row[3],)
+            print("Depth: ", row[4],)
+            print("ScreenSize: ", row[5], "\n")
+
+        
 def Display():
-    mycursor.execute("SELECT * FROM DigitalDisplay;")
+
     print("The format is: (serialNo, schedulerSystem, modelNo)")
     print("---------------------------------------------------")
-    for x in mycursor:
-      print(x)
+
+    sql = "SELECT * FROM DigitalDisplay;"
+    mycursor.execute(sql)
+    record = mycursor.fetchall()
+
+    #Check if results exist for the given query
+    if mycursor.rowcount == 0:
+        print("No results found for given query\n")
+    else:
+        for row in record:
+            print("SerialNo: ", row[0],)
+            print("SchedulerSys: ", row[1],)
+            print("ModelNo: ", row[2], "\n")
+
     
 def Search():
     search = input("Please enter the scheduler system you want to search with (either \"Random\", \"Smart\", or \"Virtue\" and without quotation marks): ")
@@ -83,14 +130,16 @@ def Logout():
     print("You have been logged out and are being sent back to the login screen.")
     Login()
 
-if __name__ == '__main__':
+def main():
     Login()
     option = ""
     while(1):
-        option = input("\n\nWelcome to your database!\nIn order to display all your devices, type \"Display\".\nTo search digital displays given a scheduler system, type \"Search\".\nTo insert a new digital display, type \"Insert\".\nTo delete a digital display, type \"Delete\".\nTo update a digital display, type \"Update\".\nTo logout of your account, type \"Logout\".\n")
+        option = input("\n\nWelcome to your database!\nIn order to display all your devices, type \"Display\".\nIn order to display all your Display Models, type \"DisplayModel\".\nTo search digital displays given a scheduler system, type \"Search\".\nTo insert a new digital display, type \"Insert\".\nTo delete a digital display, type \"Delete\".\nTo update a digital display, type \"Update\".\nTo logout of your account, type \"Logout\".\n")
         
         if(option == "Display"):
             Display()
+        elif(option == "DisplayModel"):
+            DisplayModel()
         elif(option == "Search"):
             Search()
         elif(option == "Insert"):
@@ -103,3 +152,6 @@ if __name__ == '__main__':
             Logout()
         else:
             print("I'm sorry, but that command is not available, please try again.")
+            
+if __name__ == '__main__':
+    main()
